b3772f425ebb1895dff6f32314618a2579dbba58
Josias Avram <josiasavram@gmail.com>
2022-06-20 20:07:00 -0300

    Se agrego un NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import logo from '../../src/imagenes/logo.png'
+import cart from '../../src/imagenes/cart.png'
+//import'./NavBar.css'
+
+const NavBar = () => {
+    return (
+        <header style = {styles.container}>
+        <img style = {styles.logo} src={logo} alt="" />
+        <h1 style = {styles.titulo}>PC Prodsumia</h1>
+        <nav style = {styles.navBar}>
+            <a style = {styles.anchors} href="">Categoria 1</a>
+            <a style = {styles.anchors} href="">Categoria 2</a>
+            <a style = {styles.anchors} href="">Categoria 3</a>
+        </nav>
+        <img style = {styles.carrito} src={cart} alt="" />
+        </header>
+    )
+}
+
+export default NavBar
+
+const styles = {
+    container:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        border: 'solid',
+    },
+    navBar:{
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    titulo:{
+        color: '#006dff',
+    },
+    logo:{
+        height: 50,
+        width: 100,
+    },
+    carrito:{
+        height: 30,
+        width: 30,
+    },
+    anchors:{
+        margin: 10,
+    }
+}
