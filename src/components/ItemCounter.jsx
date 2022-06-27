fb32b1fb5ea98ce82f04f4b4089e652ad7914bb6
Josias Avram <josiasavram@gmail.com>
2022-06-27 14:06:25 -0300

    Se agrega ItemCounter

---
+++ src/components/ItemCounter.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const ItemCounter = ({stock, initial = 1, onAdd}) => {
+    const [cuenta, setCuenta] = useState(initial)
+    
+    const sumar = () => {
+        cuenta < stock && setCuenta(cuenta + 1)
+        cuenta >= stock && alert('No hay stock suficiente para el producto que esta seleccionando')
+    }
+    const restar = () => {
+        setCuenta(Math.max(cuenta -1,0))
+    }
+
+  return (
+        <>
+        <div className = 'flex flex-col items-center mt-16'>
+            <p className = 'text-xl font-bold'>Placa de video Nvidia 2060</p>
+            <div className = 'flex gap-6 py-6 px-10'>
+                <button className = 'text-2xl'onClick={() => restar()}>-</button>
+                <p className = 'text-2xl font-bold'>{cuenta}</p>
+                <button className = 'text-2xl' onClick={() => sumar()}>+</button>
+            </div>
+            <button 
+                className='bg-teal-700 hover:bg-teal-900 py-3 text-white p-10'
+                onClick={onAdd}
+            >
+            Agregar Al Carrito
+            </button>
+        </div>
+        
+        </>
+  )
+}
+
+export default ItemCounter
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,23 @@
-import logo from '../../src/imagenes/logo.png'
-import Cart from './CartWidget'
-//import'./NavBar.css'
+// import logo from '../../src/assets/logo.png'
+import logo6 from '../../src/assets/logo-6.png'
+import CartWidget from './CartWidget'
 
 const NavBar = () => {
     return (
-        <header style = {styles.container}>
-        <img style = {styles.logo} src={logo} alt="" />
-        <h1 style = {styles.titulo}>PC Prodsumia</h1>
-        <nav style = {styles.navBar}>
-            <a style = {styles.anchors} href="#">Categoria 1</a>
-            <a style = {styles.anchors} href="#">Categoria 2</a>
-            <a style = {styles.anchors} href="#">Categoria 3</a>
+        <nav className = 'flex justify-center items-center bg-teal-700 py-3'>
+            <div className=' flex w-11/12 justify-between items-center'>
+                <img className = 'h-24 w-28' src={logo6} alt="" />
+                <h1 className = ' text-white font-semibold text-3xl'>PC Prodsumia</h1>
+                <ul className = 'flex gap-6 cursor-pointer text-white font-semibold'>
+                    <li className='hover:bg-teal-900 p-5 text-xl'>Inicio</li>
+                    <li className='hover:bg-teal-900 p-5 text-xl'>Productos</li>
+                    <li className='hover:bg-teal-900 p-5 text-xl'>Contacto</li>
+                </ul>
+                <CartWidget /> 
+            </div>
         </nav>
-        <Cart /> 
-        </header>
     )
 }
 
 export default NavBar
 
-const styles = {
-    container:{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        border: 'solid',
-    },
-    navBar:{
-        display: 'flex',
-        justifyContent: 'space-between',
-    },
-    titulo:{
-        color: '#006dff',
-    },
-    logo:{
-        height: 50,
-        width: 100,
-    },
-    carrito:{
-        height: 30,
-        width: 30,
-    },
-    anchors:{
-        margin: 10,
-    }
-}
