9bc3dfaea5ef00f89d55513ffc57f2d399cf080a
Josias Avram <josiasavram@gmail.com>
2022-07-27 01:24:39 -0300

    Proyecto terminado

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,31 +1,27 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../Containers/Context/CartContext";
-import { Link } from "react-router-dom";
-import { BsTrash } from "react-icons/bs";
 import { db } from "../../firebase/firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CartCheckout from "./CartCheckout";
+import { Link } from "react-router-dom";
+import { BsTrash } from "react-icons/bs";
 
 const Cart = () => {
-  const { products, removeItemFromCart, clearCart, precioTotal } =
+  const { products, clearCart, precioTotal } =
     useContext(CartContext);
 
-  const datosDelComprador = {
-    nombre: "juan",
-    apellido: "olmos",
-    email: "juanolmos@gmail.com",
-  };
 
-  const finalizarCompra = async () => {
+  const finalizarCompra = async (datos) => {
     const ventasCollection = collection(db, "ventas");
+    console.log(datos);
     const getId = await addDoc(ventasCollection, {
-      datosDelComprador,
+      datos,
       items: products,
       date: serverTimestamp(),
       total: precioTotal,
     });
-    console.log("log function", getId.id);
 
     const messageFinCompra = () => {
       toast.success(`Gracias Por su Compra, Id: ${getId.id}`, {
@@ -44,96 +40,41 @@ const Cart = () => {
 
   return (
     <>
-      {products.length === 0 ? (
+      {products.length > 0 ? 
+      ( <CartCheckout finalizarCompra = {finalizarCompra} /> ) 
+      : 
+      (
         <div>
-          <div className="bg-teal-900 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-60 p-5">
+        <div className="bg-teal-900 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-60 p-5">
             <div>Imagen</div>
             <div>Producto</div>
             <div>Precio</div>
             <div>Eliminar</div>
             <div>Cantidad</div>
             <div>Total</div>
-          </div>
+        </div>
 
-          <h1 className="text-4xl font-bold my-20 ml-5 text-center">
+        <h1 className="text-4xl font-bold my-20 ml-5 text-center">
             No hay productos en el Carrito, sigue comprando
-            <Link className="text-blue-700" to="/products">
-              {" "}
-              aquí
+            <Link className="text-blue-700 hover:underline" to="/products">
+            {" "}
+            aquí
             </Link>
-          </h1>
-          <div className="bg-teal-900 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-96 p-5">
+        </h1>
+        <div className="bg-teal-900 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-96 p-5">
             Total de la Compra
             <div className="flex">
-              <button>
+            <button>
                 <BsTrash className="mr-5" onClick={clearCart} />
-              </button>
-              <div>Vaciar Carrito</div>
+            </button>
+            <div>Vaciar Carrito</div>
             </div>
             <div>$ {precioTotal}</div>
-          </div>
         </div>
-      ) : (
-        <>
-          <div className="bg-teal-900 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-60 p-5">
-            <div>Imagen</div>
-            <div>Producto</div>
-            <div>Precio</div>
-            <div>Eliminar</div>
-            <div>Cantidad</div>
-            <div>Total</div>
-          </div>
-          <div className="mt-5">
-            {products.map((product) => (
-              <div
-                className="bg-teal-700 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-60 p-5"
-                key={product.id}
-              >
-                <img
-                  className="w-12 rounded-md ml-10"
-                  src={product.img}
-                  alt="imágen del Producto"
-                />
-                <div className="px-10 -ml-8">{product.name}</div>
-                <div className="px-10 -ml-28">$ {product.precio}</div>
-                <button>
-                  <BsTrash
-                    className=""
-                    size={30}
-                    onClick={() => removeItemFromCart(product)}
-                  />
-                </button>
-                <div className="px-10 -mr-24">
-                  {product.quantity}{" "}
-                  {product.quantity > 1 ? "unidades" : "unidad"}
-                </div>
-                <div className="px-10 -mr-10">
-                  $ {product.precio * product.quantity}
-                </div>
-              </div>
-            ))}
-            <div className="bg-teal-900 text-white rounded-xl border flex justify-between font-serif text-4xl mt-5 text-center mx-96 p-5">
-              Total de la Compra
-              <div className="flex">
-                <button>
-                  <BsTrash className="mr-3" onClick={clearCart} />
-                </button>
-                <div>Vaciar Carrito</div>
-              </div>
-              <div>$ {precioTotal}</div>
-            </div>
-            <div className="flex justify-center">
-              <button
-                className="mt-5 bg-blue-700 text-2xl text-white p-3 rounded-3xl opacity-90"
-                onClick={finalizarCompra}
-              >
-                <Link to={"/products"}>Finalizar Compra</Link>
-              </button>
-            </div>
-          </div>
-        </>
+        </div>
       )}
       <ToastContainer />
+      
     </>
   );
 };
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Form = ({ Pagar }) => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [telefono, setTelefono] = useState();
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
+
+  const datos = {
+    nombre: nombre,
+    apellido: apellido,
+    telefono: telefono,
+    email: email,
+  };
+
+  const cobrar = () => {
+    if (nombre === "" || apellido === "" || telefono === "" || email === "") {
+      return
+    } else {
+      Pagar(datos).then(navigate("/products"));
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center mt-10 text-4xl text">
+        <h1 className="bg-blue-700 p-5 mt-24 text-white rounded-2xl">
+          Datos Del Comprador
+        </h1>
+        <form>
+          <label className="underline flex justify-center mt-7">Nombre</label>
+          <input
+            className="border border-black mt-2 p-3"
+            type="text"
+            required
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder="Escribe aquí tu Nombre"
+          />
+          <br />
+
+          <label className="underline flex justify-center mt-7">
+            Apellido:
+          </label>
+          <input
+            className="border border-black mt-2 p-3"
+            type="text"
+            required
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+            placeholder="Escribe aquí tu Apellido"
+          />
+          <br />
+
+          <label className="underline flex justify-center mt-7">Tel:</label>
+          <input
+            className="border border-black mt-2 p-3"
+            type="number"
+            required
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)}
+            placeholder="1100220033"
+          />
+          <br />
+
+          <label className="underline flex justify-center mt-7">e-Mail:</label>
+          <input
+            className="border border-black mt-2 p-3"
+            type="text"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="abc@email.com"
+          />
+          <div className="flex justify-center">
+            <button
+              className="mt-7 bg-blue-700 text-2xl text-white mb-10 p-3 rounded-3xl opacity-90 px-16"
+              type="submit"
+              name="search"
+              onClick={cobrar}
+            >
+              Pagar
+            </button>
+            {/* {pagoRealizado ? <Link to={"/products"}>Pagar</Link> : <></>} */}
+          </div>
+          {/* 
+                <p>{nombre}</p><br />
+                <p>{apellido}</p><br />
+                <p>{telefono}</p><br />
+                <p>{email}</p><br /> 
+                */}
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
